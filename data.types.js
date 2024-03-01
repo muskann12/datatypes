3e046de2f9c00824765dc8748a1e0796e9fdb35f
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2024-03-01 13:03:07 -0800

    Add files via upload

---
+++ data.types.js
@@ -0,0 +1,13 @@
+//string
+var message = "hello its me muskan here";
+console.log(message);
+//number
+var numA = 20;
+var numB = 50;
+console.log(numA + numB);
+//boolean
+var isPaid = false;
+console.log(isPaid);
+//null
+var or = null;
+console.log(or);
---
+++ data.types.ts
@@ -0,0 +1,17 @@
+//string
+let message :string ="hello its me muskan here"
+console.log(message)
+
+//number
+let numA :number = 20
+let numB :number = 50
+console.log(numA + numB)
+
+//boolean
+let isPaid :boolean = false
+console.log(isPaid)
+
+//null
+let or = null
+console.log(or)
+
